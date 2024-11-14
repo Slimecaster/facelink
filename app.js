9b34a41435cf5ad951b1e94dcae2ac9852e149dc
amalieasborg <amalieasborg@gmail.com>
2024-11-14 12:30:45 +0100

    App

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require ('express')
 const mongoose = require('mongoose')
-const router = express.Router()
+const routes = require('./routes/userRoutes');
+const bodyParser =require('body-parser')
+const path = require('path');
 const port = 3000
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/facelink', {connectTimeoutMS: 10000})
+    .then(() => console.log('MongoDB Connected Successfully'))
+    .catch(err => console.error('MongoDB connection error:',err));
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true }));
+
+app.use('/',routes)
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.listen(port, () => console.log(`http://localhost:${port}`))
